e3a4c9d51dc5e663ee2f8d47282d052ff587d62b
rahulrv098 <107586112+rahulrv098@users.noreply.github.com>
2025-07-19 23:55:47 +0530

    Create AppTest.java

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        App app = new App();
+        Assert.assertEquals(5, app.add(2, 3));
+    }
+}
